f7ce41c897b46e7e5af3b44605f2ffd1a8f898a8
Mit Sananikone <Mitsananikone@gmail.com>
2023-09-07 16:57:51 -0500

    Added new pages

--- pages/index.js
+++ pages/index.js
@@ -15,15 +15,15 @@ import PricingPage from './pricing/pricingPage';
 export default function Home() {
   return (
     <>
-       <LandingPopup/> 
+      {/* <LandingPopup/> */}
       <div className="full-flex-centered">
- <LandingPage /> 
 
-
-    
         {/* <Slick/> */}
         {/* <ContactFire/> */}
-        // <PricingPage />
+        {/* <LandingPage /> */}
+
+
+        <PricingPage />
 
         {/* <div> */}
         {/* <AnimInfoCard 
